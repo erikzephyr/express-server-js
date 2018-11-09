567865c8c066f099147d31b782e67b5609679f5c
erikzephyr <erikbv99421@gmail.com>
2018-11-09 19:37:37 +0000

    exercise 3

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express();
+app.use(express.json());
+var _ = require('lodash');
 const rollDice = require('./rollDice')
 
+let users = [{id: "1", name: "Name 1"}, {id: "2", name: "Name 2"}]
+let body;
 app.get('/roll/:max?', (req, res) => {
     res.json(rollDice.roll(req.params.max));
 })
+
+app.get('/users/all', (req, res) => {
+    res.send(users);
+});
+
+app.get('/newUser', (req, res) => {
+    res.send(req.body);
+});
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    body = newUser;
+    newUser.id = _.random(0, 100);
+    users.push(newUser);
+    res.json(newUser);
+});
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
